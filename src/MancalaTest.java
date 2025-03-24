6d21413c90cf60cf6895f4d58f4fab1431756c8a
d2blepeace <nguyenhoathai.nht@gmail.com>
2025-03-24 13:09:10 -0700

    For the Player.java class: Implement Player class to contains player attributes
    For the MancalaTest.java:
    Add MancalaTest main class to test and run the game

---
+++ src/MancalaTest.java
@@ -0,0 +1,9 @@
+/**
+ * Test method to test the program
+ */
+
+public class MancalaTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Player.java
@@ -0,0 +1,18 @@
+/**
+ * Contains information about player
+ */
+public class Player {
+    private String name;
+
+    //Constructor
+    public Player(String name) {
+        this.name = name;
+    }
+    //Getters and setters
+    public void setName() {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
